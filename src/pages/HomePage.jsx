1024780918f661a9889608199f5c36b7b4ece783
Anthasss <anthasprog@gmail.com>
2025-11-21 15:28:42 +0800

    added leaflet map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 export default function HomePage() {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <h1>Welcome to the Home Page</h1>
+
+      {/* main content */}
+      <MapContainer 
+        center={[-10.1564148,123.6669857]} 
+        zoom={20} 
+        className="w-full h-full"
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   )
 }
