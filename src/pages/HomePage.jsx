5ea1cc64e538e3d13619dbc4359506a683b4f0b7
Anthasss <anthasprog@gmail.com>
2025-11-23 17:36:13 +0800

    added markers for mst building

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,42 @@
-import { MapContainer, TileLayer, GeoJSON, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, Polyline, Marker, Tooltip } from 'react-leaflet';
 import { useMemo } from 'react';
+import L from 'leaflet';
 import DistanceTable from '../components/DistanceTable';
 import { kruskalMST } from '../utils/kruskal';
 import 'leaflet/dist/leaflet.css';
+
+// Fix for default marker icon
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 export default function HomePage({ geojsonData, showMST }) {
   const mst = useMemo(() => { 
     return kruskalMST(geojsonData);
   }, [geojsonData]);
+
+  // Filter vertices that are part of MST and don't start with "Point"
+  const namedVertices = useMemo(() => {
+    if (!showMST || !mst.vertices) return [];
+    
+    // Get all vertex indices that are used in MST edges
+    const usedVertexIndices = new Set();
+    mst.edges.forEach(edge => {
+      usedVertexIndices.add(edge.fromIndex);
+      usedVertexIndices.add(edge.toIndex);
+    });
+    
+    // Filter vertices that are in MST and have names not starting with "Point"
+    return mst.vertices
+      .map((vertex, index) => ({ ...vertex, index }))
+      .filter(vertex => 
+        usedVertexIndices.has(vertex.index) && 
+        !vertex.id.startsWith('Point')
+      );
+  }, [mst, showMST]);
 
   return (
     <div className="w-full h-full flex overflow-hidden">
@@ -42,6 +71,14 @@ export default function HomePage({ geojsonData, showMST }) {
                 opacity: 0.8
               }}
             />
+          ))}
+          {/* MST Named Vertices */}
+          {showMST && namedVertices.map((vertex, index) => (
+            <Marker
+              key={index}
+              position={[vertex.coords[1], vertex.coords[0]]}
+            >
+            </Marker>
           ))}
         </MapContainer>
       </div>
